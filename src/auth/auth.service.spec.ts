1b0dda5912e777aab7d8acfd7ccf202d837ca4cb
HermanoCastro65 <hermanocastro18@gmail.com>
2022-02-12 00:48:21 -0300

    Implement auth module

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { AuthService } from './auth.service'
+import { Injectable } from '@nestjs/common'
+import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
 
-describe('AuthService', () => {
-  let service: AuthService
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile()
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username)
+    if (user && user.password === pass) {
+      const { password, ...result } = user
+      return result
+    }
+    return null
+  }
 
-    service = module.get<AuthService>(AuthService)
-  })
-
-  it('should be defined', () => {
-    expect(service).toBeDefined()
-  })
-})
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId }
+    return {
+      access_token: this.jwtService.sign(payload),
+    }
+  }
+}
